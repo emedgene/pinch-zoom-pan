89dbb2c072cf790aacb6620f497abe952653bc86
Bohdan Ilba <bohdan.ilba@gmail.com>
2021-07-13 12:31:02 +0300

    Made a main exported function a class based, to allow a good interface to work with a tree

--- src/index.ts
+++ src/index.ts
@@ -9,112 +9,119 @@ interface ICoordinates {
   z: number;
 }
 
-interface IOptions {
-  element: HTMLElement;
-  minZoom?: number;
-  maxZoom?: number;
-  captureWheel?: boolean;
-}
+export class PinchZoomPan {
+  protected _element: HTMLElement
+  protected _minZoom: number
+  protected _maxZoom: number
+  protected _captureWheel: boolean
+  protected _state: Readonly<ICoordinates>
+  protected _action: Action
+  protected _current: ICoordinates & { range: number }
+  protected _events: ReadonlyArray<{ type: any, listener: (e?: any) => void }>
+  protected readonly _touch: boolean
 
-export function create({ element, minZoom = MIN_ZOOM, maxZoom = MAX_ZOOM, captureWheel }: Readonly<IOptions>) {
-  const touch = isTouch();
+  constructor(element: HTMLElement, minZoom: number = MIN_ZOOM, maxZoom: number = MAX_ZOOM, captureWheel: boolean = false) {
+    this._state = DEFAULT_STATE
+    this._action = 'none'
+    this._minZoom = minZoom
+    this._maxZoom = maxZoom
+    this._maxZoom = maxZoom
+    this._captureWheel = captureWheel
+    this._current = { x: 0, y: 0, z: 0, range: 0 }
+    this._touch = isTouch()
+    this._element = element
+    this._events = [
+      { type: this._touch ? 'touchstart' : 'mousedown', listener: this._onStart },
+      { type: this._touch ? 'touchmove' : 'mousemove', listener: this._onMove },
+      { type: this._touch ? 'touchend' : 'mouseup', listener: this._onEnd },
+      { type: this._touch ? 'touchleave' : 'mouseleave', listener: this._onEnd },
+      { type: 'touchcancel', listener: this._onEnd },
+      { type: 'wheel', listener: this.onWheel },
+    ];
+    const { width, height } = element.getBoundingClientRect();
+    this.setState({ x: width / 2, y: height / 2, z: this._state.z });
 
-  let action: Action = 'none';
-  let state: Readonly<ICoordinates> = DEFAULT_STATE;
-  const current: ICoordinates & { range: number } = { x: 0, y: 0, z: 0, range: 0 };
-
-  // Update local state & call external set function
-  function setState(value: Readonly<ICoordinates>) {
-    state = value;
-    const point = element.children.item(0);
+    // subscribe
+    element.addEventListener(this._touch ? 'touchstart' : 'mousedown', (event: TouchEvent | MouseEvent) => this._onStart(event))
+    element.addEventListener(this._touch ? 'touchmove' : 'mousemove', (event: TouchEvent | MouseEvent) => this._onMove(event))
+    element.addEventListener(this._touch ? 'touchend' : 'mouseup', (event: TouchEvent | MouseEvent) => this._onEnd(event))
+    // @ts-ignore
+    element.addEventListener(this._touch ? 'touchleave' : 'mouseleave', (event: TouchEvent | MouseEvent) => this._onEnd(event))
+    element.addEventListener('touchcancel', (event: TouchEvent | MouseEvent) => this._onEnd(event))
+    element.addEventListener('wheel', (event: WheelEvent) => this.onWheel(event))
+  }
+  public resetState () {
+    const { width, height } = this._element.getBoundingClientRect();
+    this.setState({ x: width / 2 - 30, y: height / 2 - 175, z: this._state.z });
+    this._current = { x: 0, y: 0, z: 0, range: 0 }
+  }
+  public setState(value: Readonly<ICoordinates>) {
+    this._state = value;
+    const point = this._element.children.item(0);
     if (point) (point as HTMLElement).style.transform = `translate(${value.x}px, ${value.y}px) scale(${value.z})`;
   }
-
-  function setCurrentXY({ X, Y }: { X: number, Y: number }) {
-    current.x = X;
-    current.y = Y;
+  public setCurrentXY({ X, Y }: { X: number, Y: number }) {
+    this._current.x = X;
+    this._current.y = Y;
+  }
+  public unsubscribe() {
+    this._events.forEach(({ type, listener }) => this._element.removeEventListener(type, listener));
   }
 
-  function startMoving(event: TouchEvent | MouseEvent) {
-    action = 'move';
-    setCurrentXY(getClientXY(event));
+  protected _startMoving(event: TouchEvent | MouseEvent) {
+    this._action = 'move';
+    this.setCurrentXY(getClientXY(event));
   }
+  protected _setPosition(z: number, pageX: number, pageY: number) {
+    const { left, top } = this._element.getBoundingClientRect();
 
-  // Calculate & set new position
-  function setPosition(z: number, pageX: number, pageY: number) {
-    const { left, top } = element.getBoundingClientRect();
+    const ratio = z / this._state.z;
+    const offsetX = (pageX - left - window.pageXOffset) - this._state.x;
+    const offsetY = (pageY - top - window.pageYOffset) - this._state.y;
 
-    const ratio = z / state.z;
-    const offsetX = (pageX - left - window.pageXOffset) - state.x;
-    const offsetY = (pageY - top - window.pageYOffset) - state.y;
-
-    setState({
-      x: state.x - ((offsetX * ratio) - offsetX),
-      y: state.y - ((offsetY * ratio) - offsetY),
+    this.setState({
+      x: this._state.x - ((offsetX * ratio) - offsetX),
+      y: this._state.y - ((offsetY * ratio) - offsetY),
       z,
     });
   }
-
-  function onStart(event: TouchEvent | MouseEvent) {
+  protected _onStart(event: TouchEvent | MouseEvent) {
     event.preventDefault();
-
     if (isTouchEvent(event) && event.touches.length === 2) {
-      action = 'pinch';
-      current.z = state.z;
-      current.range = getTouchesRange(event);
+      this._action = 'pinch';
+      this._current.z = this._state.z;
+      this._current.range = getTouchesRange(event);
     }
-    else startMoving(event);
+    else this._startMoving(event);
   }
-
-  function onMove(event: TouchEvent | MouseEvent) {
+  protected _onMove(event: TouchEvent | MouseEvent) {
     event.stopImmediatePropagation();
     event.preventDefault();
 
-    if (action === 'move') {
+    if (this._action === 'move') {
       const clientXY = getClientXY(event);
-      const x = state.x - (current.x - clientXY.X);
-      const y = state.y - (current.y - clientXY.Y);
-      setCurrentXY(clientXY);
-      setState({ x, y, z: state.z });
+      const x = this._state.x - (this._current.x - clientXY.X);
+      const y = this._state.y - (this._current.y - clientXY.Y);
+      this.setCurrentXY(clientXY);
+      this.setState({ x, y, z: this._state.z });
     }
-    else if (action === 'pinch') {
-      const { scale, pageX, pageY } = getScale(event as TouchEvent, current.range);
-      const z = limitZoom(current.z * scale, minZoom, maxZoom);
-      setPosition(z, pageX, pageY);
+    else if (this._action === 'pinch') {
+      const { scale, pageX, pageY } = getScale(event as TouchEvent, this._current.range);
+      const z = limitZoom(this._current.z * scale, this._minZoom, this._maxZoom);
+      this._setPosition(z, pageX, pageY);
     }
   }
-
-  function onEnd(event: TouchEvent | MouseEvent) {
-    if (action === 'pinch' && isTouchEvent(event) && event.touches.length === 1) startMoving(event);
-    else action = 'none';
+  protected _onEnd(event: TouchEvent | MouseEvent) {
+    if (this._action === 'pinch' && isTouchEvent(event) && event.touches.length === 1) this._startMoving(event);
+    else this._action = 'none';
   }
-
-  function onWheel(event: WheelEvent) {
-    if (!captureWheel && !event.altKey) return;
+  protected onWheel(event: WheelEvent) {
+    if (!this._captureWheel && !event.altKey) return;
     event.preventDefault();
     event.stopPropagation();
 
     const delta = getWheelDelta(event) * -1;
-    const z = limitZoom(state.z + delta, minZoom, maxZoom);
-    setPosition(z, event.pageX, event.pageY);
+    const z = limitZoom(this._state.z + delta, this._minZoom, this._maxZoom);
+    this._setPosition(z, event.pageX, event.pageY);
   }
-
-  const events: ReadonlyArray<{ type: any, listener: (e?: any) => void }> = [
-    { type: touch ? 'touchstart' : 'mousedown', listener: onStart },
-    { type: touch ? 'touchmove' : 'mousemove', listener: onMove },
-    { type: touch ? 'touchend' : 'mouseup', listener: onEnd },
-    { type: touch ? 'touchleave' : 'mouseleave', listener: onEnd },
-    { type: 'touchcancel', listener: onEnd },
-    { type: 'wheel', listener: onWheel },
-  ];
-
-  // init
-  const { width, height } = element.getBoundingClientRect();
-  setState({ x: width / 2, y: height / 2, z: state.z });
-
-  // subscribe
-  events.forEach(({ type, listener }) => element.addEventListener(type, listener));
-
-  // unsubscribe
-  return () => events.forEach(({ type, listener }) => element.removeEventListener(type, listener));
 }
